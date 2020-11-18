fb8ea87a4b36ccd2a06688b608127d1551ec10f5
Jedidiah Hwang <jedidiah.hwang@gmail.com>
2020-11-18 14:23:33 -0700

    app-2 finished

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor () {
+    super ();
+
+    this.state = {
+      names: ["Gandalf", "Saruman", "Radagast"]
+    }
+  }
+
+
+  render () {
+    let wizards = this.state.names.map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+
+
+    return (
+      <div className="App">
+        {wizards}
+      </div>
+    );
+  }
+
 }
 
-export default App;
